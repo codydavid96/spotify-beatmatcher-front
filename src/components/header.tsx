c0e5fc41161e5bf8ca044bfb380adc0f5ff1af8d
Cody Hardman <codydavidh96@gmail.com>
2023-11-21 13:43:08 +1000

    Initial commit and sync.

---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+
+export default function Header() {
+  return (
+    <div className="fixed pr-10 pt-10 top-0 w-screen flex p-2 justify-end">
+      {/* <button className="py-2 px-4 bg-white hover:text-[#191414] hover:bg-[#1db954] hover:text-white">
+        Login with Spotify
+      </button> */}
+    </div>
+  );
+};
